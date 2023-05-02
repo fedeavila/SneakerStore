e986e4a2d7a50369b6b3e7309df915889d22a5f1
fedeavila <fedeavila@outlook.com>
2023-05-02 02:02:31 -0300

    Traduccion a español + retoques varios en el CSS.

--- SneakerStore/Models/Sneaker.cs
+++ SneakerStore/Models/Sneaker.cs
@@ -8,6 +8,7 @@ namespace SneakerStore.Models
 
         [Required(ErrorMessage = "El modelo es requerido!")]
         [StringLength(50, ErrorMessage = "El modelo debe tener entre {2} y {1} caracteres!", MinimumLength = 3)]
+        [Display(Name = "Modelo")]
         public string Model { get; set; }
 
         [Required(ErrorMessage = "La marca es requerida!")]
@@ -19,6 +20,8 @@ namespace SneakerStore.Models
         [Display(Name = "Precio")]
         [Range(1, int.MaxValue, ErrorMessage = "El precio debe ser mayor a cero!")]
         public decimal Price { get; set; }
+
+        [Display(Name = "Marca")]
         public virtual Brand? Brand { get; set; }
     }
 }
